8ed09ba593ddc2f663cedab98a60b0711c490b45
Vladislav Serov <serov.vladsilaw@yandex.ru>
2023-12-17 12:40:03 +0300

    add stylelint, add i18next eslint, fix styles

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,17 +1,22 @@
 import { FC } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink';
+import { useTranslation } from 'react-i18next';
 import cls from './Navbar.module.scss';
 
 interface NavBarProps {
     className?: string;
 }
 
-export const Navbar: FC<NavBarProps> = ({ className }) => (
-    <div className={classNames(cls.navbar, {}, [className])}>
-        <div className={cls.links}>
-            <AppLink theme={AppLinkTheme.SECONDARY} to='/' className={cls['main-link']}>Главная</AppLink>
-            <AppLink theme={AppLinkTheme.SECONDARY} to='/about'>О сайте</AppLink>
+export const Navbar: FC<NavBarProps> = ({ className }) => {
+    const { t } = useTranslation();
+
+    return (
+        <div className={classNames(cls.navbar, {}, [className])}>
+            <div className={cls.links}>
+                <AppLink theme={AppLinkTheme.SECONDARY} to='/' className={cls['main-link']}>{t('Main')}</AppLink>
+                <AppLink theme={AppLinkTheme.SECONDARY} to='/about'>{t('About')}</AppLink>
+            </div>
         </div>
-    </div>
-);
+    );
+};
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -3,6 +3,7 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { ThemeSwitcher } from 'shared/ui/ThemeSwitrcher';
 import { LangSwitcher } from 'shared/ui/LangSwitcher';
 
+import { useTranslation } from 'react-i18next';
 import cls from './Sidebar.module.scss';
 
 interface SidebarProps {
@@ -10,6 +11,7 @@ interface SidebarProps {
 }
 
 export const Sidebar: FC<SidebarProps> = ({ className }) => {
+    const { t } = useTranslation();
     const [collapsed, setCollapsed] = useState(false);
 
     const onToggle = () => {
@@ -24,7 +26,7 @@ export const Sidebar: FC<SidebarProps> = ({ className }) => {
                 type='button'
                 onClick={onToggle}
             >
-                toggle
+                {t('Toggle')}
             </button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
