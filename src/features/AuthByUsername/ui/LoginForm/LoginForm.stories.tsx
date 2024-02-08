2c6ee42adc4fcd4724c18c8be343c9dc61f660d0
Vladislav Serov <serov.vladsilaw@yandex.ru>
2024-02-08 23:05:34 +0300

    add login form stories and lang switcher

---
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { themeDecorator } from 'shared/config/storybook/themeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+import { LoginForm } from './LoginForm';
+
+const meta = {
+    title: 'features/LoginForm',
+    component: LoginForm,
+    parameters: {},
+} satisfies Meta<typeof LoginForm>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+    args: {},
+};
+
+export const Dark: Story = {
+    args: {},
+};
+Dark.decorators = [themeDecorator(Theme.DARK)];
---
+++ src/shared/ui/LangSwitcher/LangSwitcher.stories.tsx
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { themeDecorator } from 'shared/config/storybook/themeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+import { LangSwitcher } from './LangSwitcher';
+
+const meta = {
+    title: 'shared/LangSwitcher',
+    component: LangSwitcher,
+    parameters: {},
+} satisfies Meta<typeof LangSwitcher>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+    args: {},
+};
+
+export const PrimaryShort: Story = {
+    args: {
+        short: true,
+    },
+};
+
+export const Dark: Story = {
+    args: {},
+};
+Dark.decorators = [themeDecorator(Theme.DARK)];
