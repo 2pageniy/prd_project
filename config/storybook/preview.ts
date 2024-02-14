9f7a187ec64acf4ab593059acd1cde3cffb0de2c
Vladislav Serov <serov.vladsilaw@yandex.ru>
2024-02-14 22:54:26 +0300

    fix storybook

--- config/storybook/preview.ts
+++ config/storybook/preview.ts
@@ -1,6 +1,6 @@
 import type { Preview } from '@storybook/react';
 import 'app/styles/index.scss';
-import { themeDecorator, routerDecorator, storeDecorator } from '../../src/shared/config/storybook';
+import { routerDecorator, storeDecorator, themeDecorator } from '../../src/shared/config/storybook';
 import { Theme } from '../../src/app/providers/ThemeProvider';
 
 const preview: Preview = {
@@ -15,8 +15,8 @@ const preview: Preview = {
     },
     decorators: [
         (Story) => themeDecorator(Theme.LIGHT)(Story),
-        (Story) => routerDecorator(Story),
         (Story) => storeDecorator()(Story),
+        (Story) => routerDecorator(Story),
     ],
 };
 
