749db296bba8db41e930e74f2947e448e0ce5234
Vladislav Serov <serov.vladsilaw@yandex.ru>
2024-04-28 23:25:41 +0300

    add data virtualization for articles, fix screenshot test

--- src/entities/Article/ui/ArticleList/ArticleList.tsx
+++ src/entities/Article/ui/ArticleList/ArticleList.tsx
@@ -1,13 +1,16 @@
-import { HTMLAttributeAnchorTarget, memo } from 'react';
+import {
+    ForwardedRef, forwardRef, HTMLAttributeAnchorTarget, memo,
+} from 'react';
+import { GridListProps, Virtuoso, VirtuosoGrid } from 'react-virtuoso';
 import { useTranslation } from 'react-i18next';
-import { classNames } from 'shared/lib/classNames/classNames';
+import { ArticleListItem } from 'entities/Article/ui/ArticleListItem/ArticleListItem';
 import {
     ArticleListItemSkeleton,
 } from 'entities/Article/ui/ArticleListItem/ArticleListItemSkeleton/ArticleListItemSkeleton';
-import { Article, ArticleView } from '../../model/types/article';
-import { ArticleListItem } from '../ArticleListItem/ArticleListItem';
-
+import { ArticlePageFilters } from 'pages/ArticlesPage/ui/ArticlesPageFilters/ArticlePageFilters';
+import { classNames } from 'shared/lib/classNames/classNames';
 import cls from './ArticleList.module.scss';
+import { Article, ArticleView } from '../../model/types/article';
 
 interface ArticleListProps {
     className?: string;
@@ -15,6 +18,7 @@ interface ArticleListProps {
     isLoading?: boolean;
     view?: ArticleView;
     target?: HTMLAttributeAnchorTarget;
+    onLoadNextPart?: () => void;
 }
 
 const getSkeletons = (view: ArticleView) => Array(view === ArticleView.BIG ? 3 : 9).fill(0).map((_, index) => (
@@ -26,16 +30,62 @@ const getSkeletons = (view: ArticleView) => Array(view === ArticleView.BIG ? 3 :
     />
 ));
 
-export const ArticleList = memo(({
-    articles,
-    isLoading,
-    view = ArticleView.SMALL,
-    target,
-    className,
-}: ArticleListProps) => {
+const Header = () => (
+    <ArticlePageFilters className={cls.header} />
+);
+
+const List = forwardRef(({
+    style,
+    children,
+    ...props
+}: GridListProps, ref: ForwardedRef<HTMLDivElement>) => (
+    <div
+        ref={ref}
+        style={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            gap: 30,
+            marginRight: 30,
+            ...style,
+        }}
+        {...props}
+    >
+        {children}
+    </div>
+));
+
+interface FooterProps {
+    isLoading: boolean;
+    view: ArticleView;
+    articles: Article[];
+}
+
+const Footer = ({ isLoading, view, articles }: FooterProps) => memo(() => {
     const { t } = useTranslation();
 
-    const renderArticle = (article: Article) => {
+    return (
+        <footer
+            className={cls['skeleton-footer']}
+        >
+            {isLoading && (
+                getSkeletons(view)
+            )}
+            {!isLoading && articles.length === 0 && (
+                t('No articles')
+            )}
+        </footer>
+    );
+});
+
+export const ArticleList = memo(({
+    articles,
+    view = ArticleView.SMALL,
+    isLoading = false,
+    target,
+    onLoadNextPart = () => {},
+    className,
+}: ArticleListProps) => {
+    const renderArticle = (_: number, article: Article) => {
         return (
             <ArticleListItem
                 key={article.id}
@@ -48,16 +98,33 @@ export const ArticleList = memo(({
     };
 
     return (
-        <div className={classNames(cls['article-list'], {}, [className, cls[view]])}>
-            {articles.length > 0 ? (
-                articles.map(renderArticle)
+        <div
+            className={classNames(cls['article-list'], {}, [className, cls[view]])}
+        >
+            {ArticleView.SMALL ? (
+                <VirtuosoGrid
+                    style={{ height: '100%' }}
+                    data={articles}
+                    itemContent={renderArticle}
+                    endReached={onLoadNextPart}
+                    overscan={18}
+                    components={{
+                        Header,
+                        Footer: Footer({ isLoading, view, articles }),
+                        List,
+                    }}
+                />
             ) : (
-                !isLoading && t('No articles')
-            )}
-            {isLoading && (
-                <div className={classNames(cls['article-list'], {}, [className, cls[view]])}>
-                    {getSkeletons(view)}
-                </div>
+                <Virtuoso
+                    style={{ height: '100%' }}
+                    data={articles}
+                    itemContent={renderArticle}
+                    endReached={onLoadNextPart}
+                    components={{
+                        Header,
+                        Footer: Footer({ isLoading, view, articles }),
+                    }}
+                />
             )}
         </div>
     );
--- src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
@@ -1,18 +1,16 @@
 import { FC, memo, useCallback } from 'react';
 import { useSearchParams } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
-import { ArticleList } from 'entities/Article';
-import { classNames } from 'shared/lib/classNames/classNames';
 import { DynamicModuleLoader, ReducersList } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
 import { useInitialEffect } from 'shared/lib/hooks/useInitialEffect/useInitialEffect';
 import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
-import { Page } from 'widgets/Page';
 import { initArticlesPage } from 'pages/ArticlesPage/model/services/initArticlesPage/initArticlesPage';
+import { Page } from 'widgets/Page';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { ArticleList } from 'entities/Article';
 import { getArticlePageIsLoading, getArticlePageView } from '../../model/selectors/articlesPageSelector';
 import { articlesPageReducer, getArticles } from '../../model/slices/articlesPageSlice';
 import { fetchNextArticlesPage } from '../../model/services/fetchNextArticlesPage/fetchNextArticlesPage';
-import { ArticlePageFilters } from '../ArticlesPageFilters/ArticlePageFilters';
 
 import cls from './ArticlesPage.module.scss';
 
@@ -25,7 +23,6 @@ const reducers: ReducersList = {
 };
 
 export const ArticlesPage: FC<ArticlesPageProps> = ({ className }) => {
-    const { t } = useTranslation('article');
     const dispatch = useAppDispatch();
     const articles = useSelector(getArticles.selectAll);
     const view = useSelector(getArticlePageView);
@@ -46,12 +43,10 @@ export const ArticlesPage: FC<ArticlesPageProps> = ({ className }) => {
     return (
         <DynamicModuleLoader reducers={reducers} removeAfterUnmount={false}>
             <Page
-                onScrollEnd={onLoadNextPart}
                 className={classNames(cls['articles-page'], {}, [className])}
             >
-                {t('Articles page')}
-                <ArticlePageFilters />
                 <ArticleList
+                    onLoadNextPart={onLoadNextPart}
                     isLoading={isLoading}
                     view={view}
                     articles={articles}
